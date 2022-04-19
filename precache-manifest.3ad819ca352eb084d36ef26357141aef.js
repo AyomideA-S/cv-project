505c2bd8cfafcb55561cf02317e7be6c6c02cf15
Ayomide Ayodele-Soyebo <midesuperbest@gmail.com>
2022-04-19 14:58:50 +0100

    Updates

---
+++ precache-manifest.3ad819ca352eb084d36ef26357141aef.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f6a0845da1717da190cf",
+    "url": "/cv-project/static/css/main.090fabeb.chunk.css"
+  },
+  {
+    "revision": "f6a0845da1717da190cf",
+    "url": "/cv-project/static/js/main.f6a0845d.chunk.js"
+  },
+  {
+    "revision": "ca34b166d51c76b7fcff",
+    "url": "/cv-project/static/js/1.ca34b166.chunk.js"
+  },
+  {
+    "revision": "191a181b6a79c31ed82c",
+    "url": "/cv-project/static/js/runtime~main.191a181b.js"
+  },
+  {
+    "revision": "e980589188a9799eecbc0345528d61ab",
+    "url": "/cv-project/index.html"
+  }
+];
